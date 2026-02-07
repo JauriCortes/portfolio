f972015bf5dbed259da75988d2741c42d0468f49
jcortesca <jcortesca@unal.edu.co>
2026-02-06 23:08:27 -0500

    Boton de modo nocturno

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,32 +1,12 @@
-import { useState, useEffect } from "react";
-import './App.css'
+import DarkModeButton from "./components/DarkModeButton";
+
+import MainWindow from "./components/MainWindow";
 
 function App() {
-
-  const [theme, setTheme] = useState('light')
-
-  const toggleTheme = () => {
-    const nextTheme = theme === 'light' ? 'dark' : 'light'
-    setTheme(nextTheme)
-  }
-
-  useEffect(() => {
-    document.documentElement.setAttribute('data-theme', theme)
-  }, [theme])
-
   return (
-    <div style={{
-      height: '100vh',
-      display: 'flex',
-      flexDirection: 'column',
-      justifyContent: 'center',
-      alignItems: 'center'
-    }}>
-      <h1>Mi escritorio</h1>
-
-      <button onClick={toggleTheme} style={{ padding: '10px 20px', cursor: 'pointer'}}>
-        Pasar a modo {theme === 'light' ? 'Oscuro': 'Claro'}
-      </button>
+    <div>
+      <DarkModeButton/>
+      <MainWindow/>
     </div>
   )
 }
---
+++ frontend/src/components/MainWindow.tsx
@@ -0,0 +1,10 @@
+
+function MainWindow() {
+    return(
+        <div className="main-window">
+            hola
+        </div>
+    )
+}
+
+export default MainWindow
